28a3235717edc66c45f00c20d7bc13ae5247ff95
Saitejeshwar Erra <saitejeshware1@gmail.com>
2025-01-06 09:39:29 +0530

    initialized

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+                     //int a = 10;
+                     System.out.println(Byte.MIN_VALUE);
+                     System.out.println(Byte.MAX_VALUE);
+                     System.out.println(Integer.MIN_VALUE);
+                     System.out.println(Integer.MAX_VALUE);
+                     System.out.println("MAx value of Interger: "+ (Integer.MAX_VALUE + 1));
+                     System.out.println("Min value of Interger: "+ (Integer.MIN_VALUE + 1));
+                     System.out.println(Character.MIN_VALUE);
+                     System.out.println(Character.MAX_VALUE);
+                     System.out.println(Float.MIN_VALUE);
+                     System.out.println(Float.MAX_VALUE);
+                     System.out.println(Double.MIN_VALUE);
+                     System.out.println(Double.MAX_VALUE);
+                int b = (int) 20.9; //20
+                System.out.println(b);
+                double dl = 100.2;
+                System.out.println(dl); //100.2
+                float f = 2.9F;
+                System.out.println(f);// 2.9
+                double d = 10/3; //3.333
+                System.out.println(d);
+                
+        
+        
+                if (true){
+                    System.out.println("equal");
+                } else {
+                    System.out.println("false");
+                }    
+    }
+}
---
+++ src/Day1.java
@@ -0,0 +1,12 @@
+public class Day1 {
+    public static void main(String[] args) {
+                
+        int x = 15;
+        int y = 23;
+        if(x > y){
+            System.out.println("x is greater than y"); 
+        } else {
+            System.out.println("y is greater than x");
+        }
+    }
+}
